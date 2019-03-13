d8a880b90438e6d6782e413b84504fc88f5e95de
Lorenzo Llamas <ltl924@gmail.com>
2019-03-13 16:52:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,10 @@
 def countdown(number_of_seconds)
   counter = 0  
   while counter < number_of_seconds
-    break if 
+    if number_of_seconds == 12
+      puts "HAPPY NEW YEAR!"
+      break
+    end
     puts "#{number_of_seconds - counter} SECOND(S)!"
     counter += 1
   end
