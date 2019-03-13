41e2f98b1405f35a23b371559a35b56ed0c5a07d
Lorenzo Llamas <ltl924@gmail.com>
2019-03-13 16:50:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(number_of_seconds)
+  counter = 0  
+  while counter < number_of_seconds
+    puts "#{counter - number_of_seconds} SECOND(S)!"
+    counter += 0 
+  end
+  
 end
