a622406b0d4d0fef7188b8e0092cf9e2933c6548
Lorenzo Llamas <ltl924@gmail.com>
2019-03-13 17:16:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,7 +6,7 @@ def countdown(number_of_seconds)
     puts "#{number_of_seconds - counter} SECOND(S)!"
     counter += 1
   end
-  puts "HAPPY NEW YEAR!"
+  #puts "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number_of_seconds)
